8065b817f5b7e534526b3cab29d8d981dba4c0b3
Hai Nguyen <hainguyen@esofthead.com>
2014-09-24 11:34:05 +0700

    * Update domain fields * Set default logging for testing is INFO

--- mycollab-migration/src/main/resources/db/migration/V20140923_1__Add_Generated_Key_Column_For_Project_Entities.sql
+++ mycollab-migration/src/main/resources/db/migration/V20140923_1__Add_Generated_Key_Column_For_Project_Entities.sql
@@ -2,22 +2,22 @@ ALTER TABLE `s_user`
 CHANGE COLUMN `password` `password` VARCHAR(100) CHARACTER SET 'utf8mb4' NULL DEFAULT NULL ;
 
 ALTER TABLE `m_prj_task_list` 
-ADD COLUMN `key` INT(11) NULL;
+ADD COLUMN `prjKey` INT(11) NULL;
 
 ALTER TABLE `m_prj_risk` 
-ADD COLUMN `key` INT(11) NULL;
+ADD COLUMN `prjKey` INT(11) NULL;
 
 ALTER TABLE `m_prj_problem` 
-ADD COLUMN `key` INT(11) NULL ;
+ADD COLUMN `prjKey` INT(11) NULL ;
 
 ALTER TABLE `m_prj_message` 
-ADD COLUMN `key` INT(11) NULL;
+ADD COLUMN `prjKey` INT(11) NULL;
 
 ALTER TABLE `m_tracker_component` 
-ADD COLUMN `key` INT(11) NULL;
+ADD COLUMN `prjKey` INT(11) NULL;
 
 ALTER TABLE `m_tracker_version` 
-ADD COLUMN `key` INT(11) NULL;
+ADD COLUMN `prjKey` INT(11) NULL;
 
 ALTER TABLE `m_prj_milestone` 
-ADD COLUMN `key` INT(11) NULL;
+ADD COLUMN `prjKey` INT(11) NULL;
