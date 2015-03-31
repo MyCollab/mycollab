ccd1116ddc8b043b1dcc79f96cbf0ef5a09cf169
Hai Nguyen <hainguyen@esofthead.com>
2015-03-31 11:14:54 +0700

    Update MyCollab version

--- mycollab-core/src/main/java/com/esofthead/mycollab/core/MyCollabVersion.java
+++ mycollab-core/src/main/java/com/esofthead/mycollab/core/MyCollabVersion.java
@@ -17,13 +17,13 @@
 package com.esofthead.mycollab.core;
 
 /**
- * 
+ *
  * @author MyCollab Ltd.
  * @since 4.4.0
  *
  */
 public class MyCollabVersion {
 	public static String getVersion() {
-		return "5.0.2";
+		return "5.0.3";
 	}
 }
