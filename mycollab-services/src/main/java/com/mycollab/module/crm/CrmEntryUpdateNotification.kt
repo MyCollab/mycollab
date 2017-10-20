2a7ad0631ed75516e5202fead48d4b4b2e5f482a
Hai Nguyen <hainguyen@esofthead.com>
2017-10-20 19:16:57 +0700

    Update gitignore

---
+++ mycollab-services/src/main/java/com/mycollab/module/crm/CrmEntryUpdateNotification.kt
@@ -0,0 +1,9 @@
+package com.mycollab.module.crm
+
+import com.mycollab.core.AbstractNotification
+
+/**
+ * @author MyCollab Ltd
+ * @since 6.0.0
+ */
+class CrmEntryUpdateNotification(val type:String, val typeId:String, val message: String) : AbstractNotification(AbstractNotification.SCOPE_USER, AbstractNotification.NEWS)
